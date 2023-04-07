fd6461dc6a899cd97dc808ca9924fac2417b711d
timsar2 <info.golab@gmail.com>
2023-04-07 18:43:38 +0430

    feat(them): defineapp  colors & fonts

--- src/app/shell/ui/layout/layout.component.ts
+++ src/app/shell/ui/layout/layout.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { HeaderComponent } from '@app/shell/ui/header/header.component'
 
 @Component({
   standalone: true,
+  imports: [CommonModule, HeaderComponent],
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
   constructor() {}
-
-  ngOnInit() {}
 }
