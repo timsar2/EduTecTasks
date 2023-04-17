fffb5e30f667e1bc066d5386b4efaff87e9d6564
timsar2 <info.golab@gmail.com>
2023-04-17 21:31:52 +0430

    fix(component): fixed house factors list on filter

--- src/app/pages/landing/feature/landing-page/landing-page.component.ts
+++ src/app/pages/landing/feature/landing-page/landing-page.component.ts
@@ -4,7 +4,7 @@ import { IHouseFactors } from '@app/shared/models/house-factors.model'
 import { IActionItem } from '@app/shared/ui/action-card/data-access/models/action-item.model'
 import { ICardAction } from '@app/shared/ui/action-card/data-access/models/card-action.model'
 import { HouseService } from '../../data-access/services/house.service'
-import { combineLatest, map, startWith } from 'rxjs'
+import { combineLatest, map, of, startWith } from 'rxjs'
 
 @Component({
   selector: 'edu-landing-page',
@@ -16,31 +16,29 @@ export class LandingPageComponent {
 
   private searchTerm$ = this.searchField.valueChanges.pipe(startWith(this.searchField.value))
 
-  protected filteredFactors$ = combineLatest([this.houseService.houseFactors$, this.searchTerm$]).pipe(
-    map(([factors, searchTerm]) =>
-      factors
-        ?.filter(
-          factor =>
-            (factor.labels = factor.labels.filter(
-              x =>
-                searchTerm === undefined ||
-                searchTerm === '' ||
-                x.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
-            ))
-        )
-        .filter(x => x.labels.length > 0)
-    )
-  )
-
-  protected vm$ = combineLatest({
-    filteredFactors: this.filteredFactors$
-  })
-
-  protected cardActions: IActionItem[] = [
+  private cardActions: IActionItem[] = [
     { icon: 'delete', iconColor: 'danger', action: 'delete' },
     { icon: 'edit', iconColor: 'primary', action: 'edit' },
     { icon: 'visibility_off', iconColor: 'success', action: 'visibilityOff' }
   ]
+
+  protected filteredFactors$ = combineLatest([this.houseService.houseFactors$, this.searchTerm$]).pipe(
+    map(([factors, searchTerm]) =>
+      factors
+        .map(element => {
+          return {
+            ...element,
+            labels: element.labels.filter(x => x.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
+          }
+        })
+        .filter(b => b.labels.length > 0)
+    )
+  )
+
+  protected vm$ = combineLatest({
+    filteredFactors: this.filteredFactors$,
+    cardActions: of(this.cardActions)
+  })
 
   constructor(private houseService: HouseService) {
     houseService.getHouseFactors()
